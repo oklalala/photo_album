674bebe7b1475436acc90f2369f02339d683fb81
Tony Lin <tonylin1208@gmail.com>
2017-12-01 12:21:08 +0800

    new & create in controller and veiw

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,10 @@
 ActiveRecord::Schema.define(version: 20171024134517) do
 
   create_table "photos", force: :cascade do |t|
+    t.string "title"
+    t.date "date"
+    t.text "description"
+    t.string "file_location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
