370180295d445a5b65b057143d2527e90e578049
Tony Lin <tonylin1208@gmail.com>
2017-12-01 22:22:38 +0800

    DRY the controller & _form

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,6 @@
 class PhotosController < ApplicationController
+  before_action :set_photo, except: [:index, :new, :create]
+
   def index
     @photos = Photo.all
   end
@@ -9,28 +11,32 @@ class PhotosController < ApplicationController
 
   def create
     @photo = Photo.new(photo_params)
-    @photo.save
-
-    redirect_to photos_path
+    if @photo.save
+      redirect_to photos_url
+    else
+      render  action: :new
+    end
   end
 
-  def show
-    @photo = Photo.find(params[:id])
-  end
+  # def show
+  #   @photo = Photo.find(params[:id])
+  # end
 
-  def edit
-    @photo = Photo.find(params[:id])
-  end
+  # def edit
+  #   @photo = Photo.find(params[:id])
+  # end
 
   def update
-    @photo = Photo.find(params[:id])
-    @photo.update_attributes(photo_params)
-
-    redirect_to photo_path(@photo)
+    # @photo = Photo.find(params[:id])
+    if @photo.update_attributes(photo_params)
+      redirect_to photo_path(@photo)
+    else
+      render  :action => :edit
+    end
   end
 
   def destroy
-    @photo = Photo.find(params[:id])
+    # @photo = Photo.find(params[:id])
     @photo.destroy
 
     redirect_to photos_path
@@ -40,5 +46,9 @@ class PhotosController < ApplicationController
 
   def photo_params
     params.require(:photo).permit(:title, :date, :description, :file_location)
+  end
+
+  def set_photo
+    @photo = Photo.find(params[:id])
   end
 end
